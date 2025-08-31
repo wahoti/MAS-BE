9eb086161dc7375378b1bf3a0aefff113ffea18d
wahoti <abdul@firstfoundy.co>
2025-08-31 09:35:33 -0700

    first contact

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3005;
+
+app.use(cors()); // This enables CORS for all routes and allows all origins.
 
 // Define a basic route
 app.get("/", (req, res) => {
   res.send("Hello from your Express API!");
+});
+
+// Define a basic route
+app.get("/test", (req, res) => {
+  console.log("GOT A TEST CALL");
+  res.send("HELLO TEST");
 });
 
 // Start the server
